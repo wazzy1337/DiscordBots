33733feba0b9918fc80fcb1e5167d29964b81a46
Warren Reynolds <Warren.Reynolds@tribalgroup.com>
2025-10-20 16:58:41 +0100

    - Add third-party library (Discord) - Modify Token name to make Discord bot - Set bot intents (default + message content) - Add bot commands e.g. $, Sin, SIN, sin - Add command line prompt when bot is online - Create bot command "TotalSins" (early version). - Run bot

--- SinTracker/SinTracker.py
+++ SinTracker/SinTracker.py
@@ -1,7 +1,22 @@
 import os
+
 from dotenv import load_dotenv
+import discord
+from discord.ext import commands
 
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+TOKEN = os.getenv('SIN_TRACKER_BOT_TOKEN')
 
-print(TOKEN)
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix=["$","Sin", "SIN", "sin"], intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Bot is online as {bot.user}')
+
+@bot.command()
+async def TotalSins(ctx):
+    await ctx.send("Total:")
+
+bot.run(TOKEN)
