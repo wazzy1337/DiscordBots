648fdc3f91586cb91205fea210eb073a38ca8da7
Warren Reynolds <Warren.Reynolds@tribalgroup.com>
2025-10-20 17:47:45 +0100

    - Add app_commands for @bot.tree.command functionality - Create help command to display available commands to users. - Remove help command from command list - Send ephemeral message to user in channel for a list of commands.

--- SinTracker/SinTracker.py
+++ SinTracker/SinTracker.py
@@ -2,6 +2,7 @@ import os
 
 from dotenv import load_dotenv
 import discord
+from discord import app_commands
 from discord.ext import commands
 
 load_dotenv()
@@ -13,10 +14,21 @@ bot = commands.Bot(command_prefix=["$","Sin", "SIN", "sin"], intents=intents)
 
 @bot.event
 async def on_ready():
+    await bot.tree.sync()
     print(f'Bot is online as {bot.user}')
 
-@bot.command()
-async def TotalSins(ctx):
-    await ctx.send("Total:")
+@bot.tree.command(name="help", description="Shows all available commands")
+async def help_command(interaction: discord.Interaction):
+    # Build a list of all registered slash commands excluding this (help).
+    commands_list = [
+        f"/{cmd.name} - {cmd.description}" 
+        for cmd in bot.tree.get_commands()
+        if cmd.name != "help"
+    ]
+    
+    await interaction.response.send_message(
+        "\nHere are my commands:\n" + "\n".join(commands_list),
+        ephemeral=True
+    )
 
 bot.run(TOKEN)
