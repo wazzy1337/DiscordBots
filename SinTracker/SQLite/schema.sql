f8d767b50baf366b8245ea30180fd158cfc83670
Warren Reynolds <Warren.Reynolds@tribalgroup.com>
2025-10-24 23:41:33 +0100

    - Create folder for SQLite files - Add SQLite schema file     - Create table for players (sinners) - Add schema.sql to solution

---
+++ SinTracker/SQLite/schema.sql
@@ -0,0 +1,7 @@
+﻿-- players (Sinners) table: tracks each player (sinner) and their total sins.
+CREATE TABLE IF NOT EXISTS players (
+    player_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    player_name TEXT,
+    player_nickname TEXT,
+    total_sins INTEGER DEFAULT 0 
+);
