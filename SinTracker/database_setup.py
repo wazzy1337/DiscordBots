a69ad02fe8e0563cac1d58cae2ffb70cc555b4a0
Warren Reynolds <Warren.Reynolds@tribalgroup.com>
2025-10-24 22:55:13 +0100

    - Add database setup     - Create database (sinTracker.db)     - Connect to database     - Print to console connection status and if database was just made.     - Close connection to database.

---
+++ SinTracker/database_setup.py
@@ -0,0 +1,16 @@
+﻿import os
+import sqlite3
+
+def initialize_db():
+    db_filename = "sinTracker.db"
+
+    db_exists = os.path.exists(db_filename)
+
+    conn = sqlite3.connect(db_filename)
+
+    if db_exists:
+        print(f"Connected to existing database: {db_filename}")
+    else:
+        print(f"Database created and connected: {db_filename}")
+
+    conn.close()
