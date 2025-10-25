6b6906fc8c63c723cfe6f3e4f2dfdd73a6e189bd
Warren Reynolds <Warren.Reynolds@tribalgroup.com>
2025-10-25 13:27:39 +0100

    - Create sin table (actions that gain sin points) - Add sin table to DB backup

--- SinTracker/SQLite/schema.sql
+++ SinTracker/SQLite/schema.sql
@@ -5,3 +5,11 @@ CREATE TABLE IF NOT EXISTS players (
     player_nickname TEXT,
     total_sins INTEGER DEFAULT 0 
 );
+
+-- sin table: defines each type of action a player (sinner) can be penalized for.
+CREATE TABLE IF NOT EXISTS sin (
+    sin_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    sin_name TEXT,
+    sin_description TEXT,
+    sin_points INTEGER DEFAULT 0 
+);
