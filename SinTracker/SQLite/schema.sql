9c207cbed38fb7ee4df86fd1623dec0d61e27f0b
Warren Reynolds <Warren.Reynolds@tribalgroup.com>
2025-11-17 22:02:20 +0000

    Add Discord ID to players table     - update DB for 'Wazzy'

--- SinTracker/SQLite/schema.sql
+++ SinTracker/SQLite/schema.sql
@@ -1,6 +1,7 @@
 ﻿-- players (Sinners) table: tracks each player (sinner) and their total sins.
 CREATE TABLE IF NOT EXISTS players (
     player_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    discord_id INTEGER DEFAULT 0,
     player_name TEXT,
     player_nickname TEXT,
     total_sins INTEGER DEFAULT 0 
